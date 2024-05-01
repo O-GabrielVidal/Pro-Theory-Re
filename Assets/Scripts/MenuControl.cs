188d0274191f210bcadfc7dad2ac3473a3638fdb
O-GabrielVidal <115381023+O-GabrielVidal@users.noreply.github.com>
2024-04-30 21:39:34 -0300

    Title screen
    - Created a script and gameobject for data between scenes.
    - Added a title screen and entry box for name.

--- Assets/Scripts/MenuControl.cs
+++ Assets/Scripts/MenuControl.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class MenuControl : MonoBehaviour
 {
+    public TMP_InputField inputField = null;
+
+    private void Start()
+    {
+        if(SceneManager.GetActiveScene().name== "Title")
+        {
+            inputField = GameObject.Find("Group").transform.GetChild(1).GetComponent<TMP_InputField>();
+        }
+    }
     public void Return()
     {
         GameObject.Find("Canvas").transform.GetChild(0).gameObject.SetActive(false);
